9e08313fb2ba8a2d47e985939290bacb28e2a29f
Paulyn <paulyntran@live.ca>
2019-03-06 23:36:56 +0000

    Completed aToB

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  let total = 0
+  if (fromN === toN) {
+    return fromN
+  } else if (fromN === 1){
+    return fromN + toN;
+  } else if (fromN === 0) {
+    return fromN + toN;
+  } else if (fromN < toN) {
+    for (let N = fromN; N<= toN; N++) {
+      total += N;
+    }
+    return total;
+  }
 }
 
 module.exports = sum;
